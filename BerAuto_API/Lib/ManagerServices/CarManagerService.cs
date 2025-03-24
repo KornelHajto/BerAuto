049456ee849796eb398f7f8903c5e90d378f9450
Bojsza Réka <bojsza.reka.laura@gmail.com>
2025-03-24 16:35:59 +0100

    initial setup

---
+++ BerAuto_API/Lib/ManagerServices/CarManagerService.cs
@@ -0,0 +1,51 @@
+﻿using Microsoft.Extensions.Caching.Distributed;
+
+namespace BerAuto.Lib.ManagerServices
+{
+	public class CarManagerService
+	{
+		private readonly API_DbContext _dbContext;
+		private readonly IDistributedCache _cache;
+		public CarManagerService(API_DbContext dbContext, IDistributedCache cache)
+		{
+			_dbContext = dbContext;
+			_cache = cache;
+		}
+
+		public void ListCars() 
+		{ 
+		
+		}
+
+		public void GetCar()
+		{
+
+		}
+
+		public void CreateCar()
+		{
+
+		}
+
+		public void DeleteCar()
+		{
+
+		}
+
+		public void UpdateCar()
+		{
+
+		}
+
+		public void IsAvailableOnDayInterval() {
+		
+		}
+
+		public void GetCarRentHistory()
+		{
+
+		}
+
+
+	}
+}
