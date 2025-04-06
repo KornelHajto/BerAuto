db61aa4f62954d4e3a18a93c4688fd03d9e92893
Hajtó Kornél <kornelhajto2004@gmail.com>
2025-04-06 22:45:49 +0200

    Added Controller file and List method

---
+++ BerAuto_API/Controllers/RentalController.cs
@@ -0,0 +1,38 @@
+﻿using BerAuto.Lib.ManagerServices;
+using Microsoft.AspNetCore.Mvc;
+
+namespace BerAuto_API.Controllers
+{
+
+    [Route("/api/[controller]")]
+    [ApiController]
+    public class RentalController : Controller
+    {
+        RentalManagerService rentalManager;
+
+
+        public RentalController(API_DbContext dbContext, IDistributedCache cache)
+        {
+            rentalManager = new RentalManagerService(dbContext, cache);
+        }
+
+        [HttpGet]
+        public async Task<IActionResult> ListCars()
+        {
+            ApiResponse response = new ApiResponse();
+            try
+            {
+                response.Data = await rentalManager.ListRentals();
+                return Ok(response);
+            }
+            catch (Exception e)
+            {
+                response.StatusCode = 400;
+                response.Message = e.Message;
+            }
+
+            return BadRequest(response);
+        }
+
+    }
+}
