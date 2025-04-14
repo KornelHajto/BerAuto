04938462e2fff8cc415bafc68544e185a1560935
Baloghady <baloghady04@gmail.com>
2025-04-14 10:21:05 +0200

    Creating Repository, UnitOfWork files

---
+++ BerAuto_API/Lib/Repositories/GenericRepository.cs
@@ -0,0 +1,29 @@
+﻿using BerAuto.Lib.Database;
+using Microsoft.EntityFrameworkCore;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Linq.Expressions;
+using System.Threading.Tasks;
+
+namespace BerAuto.Lib.Repositories
+{
+    public class GenericRepository<T> : IRepository<T> where T : class
+    {
+        protected readonly API_DbContext _context;
+        private readonly DbSet<T> _entities;
+
+        public GenericRepository(API_DbContext context)
+        {
+            _context = context;
+            _entities = _context.Set<T>();
+        }
+
+        public async Task<IEnumerable<T>> GetAllAsync() => await _entities.ToListAsync();
+        public async Task<T> GetByIdAsync(Guid id) => await _entities.FindAsync(id);
+        public async Task<IEnumerable<T>> FindAsync(Expression<Func<T, bool>> predicate) => await _entities.Where(predicate).ToListAsync();
+        public async Task AddAsync(T entity) => await _entities.AddAsync(entity);
+        public void Update(T entity) => _entities.Update(entity);
+        public void Remove(T entity) => _entities.Remove(entity);
+    }
+}
---
+++ BerAuto_API/Lib/Repositories/IUnitOfWork.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Threading.Tasks;
+using BerAuto.Models;
+
+namespace BerAuto.Lib.Repositories
+{
+    public interface IUnitOfWork : IDisposable
+    {
+        IRepository<Car> Cars { get; }
+        IRepository<CarRent> CarRents { get; }
+        IRepository<Category> Categories { get; }
+        IRepository<Log> Logs { get; }
+        IRepository<Rent> Rents { get; }
+        IRepository<User> Users { get; }
+        Task<int> CompleteAsync();
+    }
+}
